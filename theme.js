cf815a0529923f0358046039f8659ff1a0495026
Angela Martin <amartin3@woh.rr.com>
2021-03-22 00:39:32 -0400

    6

---
+++ theme.js
@@ -0,0 +1,11 @@
+/*
+============================================
+; Title: Assignment 1.3
+; Author: Professor Krasso
+; Date: 22 March 2021
+; Modified By: Angela Martin
+; Description: This program demonstrates the
+; use of JavaScript types, values, and
+; and variables in an application.
+;===========================================
+*/
